d04b8e1c2e82109fbe58a0b97acec99315f936f6
macchina_kali <iacopoliberolavoro@gmail.com>
2025-03-10 12:19:14 +0100

    Add initial project files including database connection, HTML structure, and deployment workflow

---
+++ connessione.php
@@ -0,0 +1,24 @@
+<?php
+    // Definizione delle variabili per la connessione al database
+    $servername = "localhost";
+    $username = "superipetizioni";
+    $password = "";
+    $dbname = "my_superipetizioni";
+
+    /*
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mercatino";
+    */
+    
+    mysqli_report(MYSQLI_REPORT_OFF);  // Serve a disabilitare le eccezioni nelle nuove versioni di PHP
+
+    // Creazione della connessione al database
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Controllo della connessione
+    if ($conn->connect_error) {
+        echo("Connection failed: " . $conn->connect_error);
+    }
+?>
