1291d7aff3315e1242d9ebffb51a29620fe5d122
macchina_kali <iacopoliberolavoro@gmail.com>
2025-03-10 23:11:46 +0100

    Update FTP password to use GitHub secrets and add Users table to database

---
+++ DB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Users (
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL PRIMARY KEY,
+    password CHAR(64) NOT NULL, -- SHA-256 produces a 64-character hash
+    is_teacher BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- Example of inserting a user without specifying is_teacher
+-- INSERT INTO Users (username, email, password) VALUES ('JohnDoe', 'john@example.com', SHA2('password', 256));
