99b9584429620ad967ec5b13c5bc05620fb4fa5e
pclab <iacopoliberolavoro@gmail.com>
2025-03-11 16:43:24 +0100

    Fix email variable in user registration query

--- login/script_registrazione.php
+++ login/script_registrazione.php
@@ -25,7 +25,7 @@
     // Se non è registrato lo inserisco nel database, altrimenti mostro un errore
     if($result->num_rows == 0)
     {
-        $query = "INSERT INTO `Users`(`username`, `email`, `password`, `is_teacher`) VALUES ('$username','$mail','$password',$professore)";
+        $query = "INSERT INTO `Users`(`username`, `email`, `password`, `is_teacher`) VALUES ('$username','$email','$password',$professore)";
         if ($conn->query($query)) 
         {
             $_SESSION['status_reg'] = "Registrazione effettuata";
