7f6da00d5ff03d11c711037983ffaf974c1a2f77
pclab <iacopoliberolavoro@gmail.com>
2025-03-11 16:52:11 +0100

    Fix SQL query syntax in user registration script

--- login/script_registrazione.php
+++ login/script_registrazione.php
@@ -25,20 +25,15 @@
     // Se non è registrato lo inserisco nel database, altrimenti mostro un errore
     if($result->num_rows == 0)
     {
-        $query = "INSERT INTO `Users`(`username`, `email`, `password`, `is_teacher`) VALUES ('$username','$email','$password',$professore)";
-        try {
-            if ($conn->query($query)) 
-            {
-                $_SESSION['status_reg'] = "Registrazione effettuata";
-                header("Location: ..\index.php");
-            } 
-            else 
-            {
-                $_SESSION['status_reg'] = "Errore nella registrazione";
-                header("Location: ..\index.php");
-            }
-        } catch (mysqli_sql_exception $e) {
-            $_SESSION['status_reg'] = "Errore nella registrazione: " . $e->getMessage();
+        $query = "INSERT INTO `Users`(`username`, `email`, `password`, `is_teacher`) VALUES ($username,$email,$password,$professore)";
+        if ($conn->query($query)) 
+        {
+            $_SESSION['status_reg'] = "Registrazione effettuata";
+            header("Location: ..\index.php");
+        } 
+        else 
+        {
+            $_SESSION['status_reg'] = "Errore nella registrazione";
             header("Location: ..\index.php");
         }
     }
